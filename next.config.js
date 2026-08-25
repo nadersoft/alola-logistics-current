f6e390688ba838eb404b86268397184e90313391
nadersoft <nadersoft2@gmail.com>
2026-08-25 16:57:54 +0300

    Modify next.config.js for Vercel output settings

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   images: { unoptimized: true },
-  // على Vercel يجب أن يكون .next، محلياً نستخدم .next-prod للإنتاج
   distDir: process.env.VERCEL ? '.next' : (process.env.NODE_ENV === 'production' ? '.next-prod' : '.next'),
+  ...(process.env.VERCEL ? {} : { output: 'standalone' }),
 };
 
 module.exports = nextConfig;
